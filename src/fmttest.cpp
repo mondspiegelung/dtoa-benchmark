fc0407cfda131dc708ee169dee4f392f5def7125
Victor Zverovich <viz@fb.com>
2020-06-20 09:36:58 -0700

    Update fmt

--- src/fmttest.cpp
+++ src/fmttest.cpp
@@ -1,9 +1,9 @@
 #define FMT_HEADER_ONLY 1
 #include "test.h"
-#include "fmt/format.h"
+#include "fmt/compile.h"
 
 void dtoa_fmt(double value, char* buffer) {
-  buffer = fmt::format_to(buffer, "{}", value);
+  buffer = fmt::format_to(buffer, FMT_COMPILE("{}"), value);
   *buffer = '\0';
 }
 
